219fc05d7bf0be1afa574fb557b201c9d58958dc
Michael Sarpong <mike.sarp.ky@gmail.com>
2019-10-14 15:10:00 +0000

    initial commit

---
+++ precache-manifest.5bac3bd13ead0d3aa1fa54739ae86e39.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c690929eb3b00989408bbb4fb0a35e09",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2c0836e6ea4162b5b49",
+    "url": "/static/css/2.f6fac5cc.chunk.css"
+  },
+  {
+    "revision": "c31f6fb0a37698249182",
+    "url": "/static/css/main.85ae8d7e.chunk.css"
+  },
+  {
+    "revision": "a2c0836e6ea4162b5b49",
+    "url": "/static/js/2.24cb1ea7.chunk.js"
+  },
+  {
+    "revision": "c31f6fb0a37698249182",
+    "url": "/static/js/main.62bd4966.chunk.js"
+  },
+  {
+    "revision": "b732ca256bf8411160ec",
+    "url": "/static/js/runtime~main.55ec1482.js"
+  },
+  {
+    "revision": "46a67389d7760f3d394f6fc425406732",
+    "url": "/static/media/AlexLoup.46a67389.jpg"
+  },
+  {
+    "revision": "6b2e1f2c25ef6d26110d1d96649664cf",
+    "url": "/static/media/AnthonyMelone.6b2e1f2c.jpg"
+  },
+  {
+    "revision": "55dc8b1354b68dfd1d82e6067acfba6e",
+    "url": "/static/media/DanielSalgado.55dc8b13.jpg"
+  }
+]);
